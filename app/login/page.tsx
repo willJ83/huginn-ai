2864f25e866e199954ee0bb6530b723437a51a2c
Will Johnson <odenseyecreative@gmail.com>
2026-03-25 14:58:29 -0400

    fix(login): prevent prerender failure on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
